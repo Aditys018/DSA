53d9fd368f47f61895498037772863a7e08c5b3c
Aditi Shinde <aaditishinde1804@gmail.com>
2024-10-21 21:58:37 +0530

    code snippets1

---
+++ Assignment_No1/Qu1.c
@@ -0,0 +1,30 @@
+/*  COMPLETE BELOW CODE SNIPPETS
+
+1. Program to divide two numbers
+
+#include<stdio.h>
+________Divide(int iNo1, int iNo2)
+{
+    int iAns = 0;
+
+    if(ino2________)
+    {
+        return -1;
+    
+    }
+
+    iAns = iNo1 / iNo2;
+    return_______;
+
+}
+int main()
+{
+    int iValue1 = 15, iValue2 = 5;
+    int iRet = Divide(______,_________);
+    printf("Division is %d" , _________);
+    return 0;
+
+}
+*/
+
+
