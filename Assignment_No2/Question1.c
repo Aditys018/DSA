0a25623736abc97b762a3a69382350140622f594
Aditi Shinde <aaditishinde1804@gmail.com>
2024-10-22 22:23:26 +0530

    Assignment2

---
+++ Assignment_No2/Question1.c
@@ -0,0 +1,60 @@
+/*
+1. Accept one number from user and print that number of * on screen.
+________________________________________________________________________
+
+void Display(int iNo)
+{
+
+int iCnt = 0;
+//write updater
+while(iCnt<__________)
+{
+    printf("*");
+    _________;
+}
+
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number");
+    scanf("_____", &________);
+
+    Display(iValue);
+
+    return 0;
+
+}
+
+*/
+
+
+void Display(int iNo)
+{
+
+int iCnt = 0;
+//write updater
+while(iCnt<__________)
+{
+    printf("*");
+    _________;
+}
+
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number");
+    scanf("_____", &________);
+
+    Display(iValue);
+
+    return 0;
+
+}
---
+++ Assignment_No2/Question2.c
@@ -0,0 +1,54 @@
+/*
+2. Accept one number from user and print that number of * on screen
+___________________________________________________________________________
+
+void Display(int iNo)
+{
+    //write updater
+    while(iNo>__________)
+    {
+        printf("*");
+        iNo___;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number");
+    scanf("_______", &_________);
+
+
+    Display(iValue);
+
+    return 0;
+
+
+
+}
+
+*/
+
+void Display(int iNo)
+{
+    //write updater
+    while(iNo>__________)
+    {
+        printf("*");
+        iNo___;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number");
+    scanf("_______", &_________);
+
+
+    Display(iValue);
+
+    return 0;
+}
