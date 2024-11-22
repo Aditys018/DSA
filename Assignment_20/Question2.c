d8ecf4adc4123185f0e166925388d62f4a53b410
Aditi Shinde <aaditishinde1804@gmail.com>
2024-11-23 01:38:41 +0530

    Amend

--- Assignment_20/Question2.c
+++ Assignment_20/Question2.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+
 void pattern(int iRow, int iCol)
 {
     
@@ -9,7 +10,7 @@ int main()
     int iValue1 = 0, iValue2 = 0;
 
     printf("Enter number of rows and columns:\n");
-    scanf("%d%d" , &iValue1, &iValue2);
+    scanf("%d" , &iValue1, &iValue2);
 
     pattern(iValue1, iValue2);
 
--- Assignment_20/Question3.c
+++ Assignment_20/Question3.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+
 void pattern(int iRow, int iCol)
 {
     
@@ -9,9 +10,9 @@ int main()
     int iValue1 = 0, iValue2 = 0;
 
     printf("Enter number of rows and columns:\n");
-    scanf("%d%d" , &iValue1, &iValue2);
+    scanf("%d%d", &iValue1, &iValue2);
 
     pattern(iValue1, iValue2);
-
     return 0;
+
 }
