9e6918c28d4fca2050576441fc8b3af9a79b583e
Aditi Shinde <aaditishinde1804@gmail.com>
2025-01-24 22:52:31 +0530

    Amend

---
+++ Assignment_No9/Question1.c
@@ -0,0 +1,34 @@
+/*
+write a program which accept radius of circle from user and calculate its area.
+consider value of PI as 3.14(Area = PI *Radius * Radus)
+
+Input: 5.3
+Output : 88.2026
+
+Input : 10.4
+Output : 339.6224
+
+
+#include<stdio.h>
+
+double CircleArea(float fRadius)
+{
+    //Logic
+}
+
+int main()
+{
+    float fValue = 0.0;
+    double dRet = 0.0;
+
+    printf("Enter radius");
+    scanf("%f", &fValue);
+
+    dRET = CircleArea(fValue);
+
+    printf("------------");
+
+    return 0;
+}
+
+*/
